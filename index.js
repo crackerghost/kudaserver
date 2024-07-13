d478bd08383780cad52d4148c9c338bc1151ce99
RAJ <singhrajtilak64@gmail.com>
2024-07-13 19:11:32 +0530

    AD

--- index.js
+++ index.js
@@ -98,7 +98,7 @@ app.post("/register", async (req, res) => {
         lastName,
         kyc,
         email,
-        hashedPass,
+        password: hashedPass,
         role,
         idProof,
     });
